41525281610394305096f50f08859c9ed8a5137c
Michael Thorpe <michaelthorpe747@hotmail.com>
2020-09-05 20:11:38 +0100

    responsive header designs and mobeil navigation

---
+++ scoot-multi-page-website/script.js
@@ -0,0 +1,15 @@
+var hamburger = document.querySelector('.hamburger');
+var mobileMenu = document.querySelector('.mobile-nav');
+var mobileMenuBg = document.querySelector('.mobile-nav-bg');
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-nav-active');
+    hamburger.classList.toggle('hamburger-close');
+    mobileMenuBg.classList.toggle('mobile-nav-bg-active');
+});
+
+mobileMenuBg.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile-nav-active');
+    hamburger.classList.remove('hamburger-close');
+    mobileMenuBg.classList.remove('mobile-nav-bg-active');
+});
