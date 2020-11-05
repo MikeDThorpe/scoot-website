6984f2ff0c1b7257b27e0ab43980c9a7be1422c9
Michael Thorpe <michaelthorpe747@hotmail.com>
2020-11-05 13:30:11 +0000

    adds files for about page

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var hamburger = document.querySelector('.hamburger');
-var mobileMenu = document.querySelector('.mobile-nav');
-var mobileMenuBg = document.querySelector('.mobile-nav-bg');
+let hamburger = document.querySelector('.hamburger');
+let mobileMenu = document.querySelector('.mobile-nav');
+let mobileMenuBg = document.querySelector('.mobile-nav-bg');
+let header = document.querySelector('header');
 
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile-nav-active');
     hamburger.classList.toggle('hamburger-close');
     mobileMenuBg.classList.toggle('mobile-nav-bg-active');
+    header.style.position = "fixed";
 });
 
 mobileMenuBg.addEventListener('click', () => {
     mobileMenu.classList.remove('mobile-nav-active');
     hamburger.classList.remove('hamburger-close');
-    hamburger.
     mobileMenuBg.classList.remove('mobile-nav-bg-active');
+    header.style.position = "relative";
 });
