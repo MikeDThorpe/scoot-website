8e1ce92e8126fe37f92c368659d302df571f7d60
Michael Thorpe <michaelthorpe747@hotmail.com>
2020-11-11 14:34:06 +0000

    adds faq section to about us page

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile-nav-active');
     hamburger.classList.toggle('hamburger-close');
     mobileMenuBg.classList.toggle('mobile-nav-bg-active');
-    header.style.position = "fixed";
+    header.classList.toggle("hdr-fixed");
 });
 
 mobileMenuBg.addEventListener('click', () => {
@@ -16,3 +16,24 @@ mobileMenuBg.addEventListener('click', () => {
     mobileMenuBg.classList.remove('mobile-nav-bg-active');
     header.style.position = "relative";
 });
+
+let faqRow = document.querySelectorAll(".faq-title");
+let arrow = document.querySelectorAll(".faq-arrow");
+
+for (let i = 0; i < faqRow.length; i++) {
+    faqRow[i].addEventListener("click", () => {
+        console.log(faqRow[i]);
+        let focusFaq = faqRow[i];
+       focusFaq .classList.toggle("active");
+       arrow[i].classList.toggle("arrow-active");
+
+    //    open FAQ answer when title container is clicked
+       let answer = focusFaq.nextElementSibling;
+        console.log(answer);
+        if(answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    })
+}
